6f4b00c39d88fbee6ea99dbd0a5b2d890d7a2023
ozieblo <59211762+ozieblo-michal@users.noreply.github.com>
2024-03-05 23:40:00 +0100

    combining Rust and Python

--- scrapper/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-
-    let bytes = std::fs::read("file.pdf").unwrap();
-    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
-    assert!(out.contains("This is a small demonstration"));
-
-}
